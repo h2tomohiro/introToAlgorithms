bc448793a490e9a0f6ac33d31632d4954061703b
Tomohiro Meo <h2.adachi.tomohiro@gmail.com>
2020-09-30 02:22:08 -0700

    Set permutation

--- Labs/group_lab2.py
+++ Labs/group_lab2.py
@@ -9,8 +9,29 @@ def permutation(word: str):
     :param word: word to permute
     :return: display permutations of a given word
     """
-    pass
+    # if len(word) < 2:
+    #     yield word
+    #     return
+    # for pos in range(0, len(word)):
+    #     char = word[pos]
+    #     Remaining = list(permutation(word[0:pos] + word[pos + 1:]))
+    #     for perm in Remaining:
+    #         yield char + perm
+    if len(word) == 0:
+        return ['']
+    prevList = permutation(word[1:len(word)])
+    nextList = []
+    for i in range(0,len(prevList)):
+        for j in range(0,len(word)):
+            newString = prevList[i][0:j]+word[0]+prevList[i][j:len(word)-1]
+            if newString not in nextList:
+                #print(newString)
+                nextList.append(newString)
+    return nextList
 
+    # 順列の数だけ存在する？
+    # 文字の数だけ繰り返す？
+print(permutation("mark"))
 
 def sum_of_dice(dice: int, desired_sum: int):
     """
@@ -25,4 +46,9 @@ def sum_of_dice(dice: int, desired_sum: int):
     output: {1, 6}, {2, 5}, {3, 4}, {4, 3}, {5, 2}, {6, 1}
 
     """
+    #sum_of_dice(1, 6) {6}
+    #sum_of_dice(3, 4) {1,1,2},{1,2,1},{2,1,1}
+    # サイコロの数
+    # サイコロの数で実現できるパターンを表示
+
     pass
